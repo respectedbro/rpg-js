5565fb78160707b3791a2c251d3b9f501ff4405c
NikDm <respectedoff@yandex.ru>
2025-05-18 21:43:12 +0300

    combat

---
+++ data.js
@@ -0,0 +1,92 @@
+const charInputName = document.getElementById('name');
+const startGameButton = document.getElementById('startGame');
+const startInfo = document.querySelector('.start__info');
+const rpgWrapper = document.querySelector('.rpg__wrapper ');
+const propertyNames = {
+  name: 'Имя',
+  type: 'Раса',
+  health: 'Здоровье',
+  strength: 'Сила',
+  defense: 'Защита',
+  level: 'Уровень',
+  inventory: 'Инвентарь'
+};
+
+const raceStats = {
+  human: {type: 'Человек', health: 10, strength: 2, defense: 2},
+  elf: {type: 'Эльф', health: 8, strength: 3, defense: 1},
+  dwarf: {type: 'Гном', health: 12, strength: 1, defense: 3}
+};
+
+const userChar = {
+  name: '',
+  type: '',
+  health: 0,
+  strength: 0,
+  defense: 0,
+  level: 1,
+  inventory: []
+};
+
+const enemies = {
+  orc: {
+    name: 'Орк',
+    health: 5,
+    strength: 1,
+    defense: 2
+  },
+  troll: {
+    name: 'Троль',
+    health: 3,
+    strength: 2,
+    defense: 3
+  }
+};
+
+startGameButton.addEventListener('click', () => {
+
+  if (!charInputName.value) {
+    alert('Введите имя персонажа');
+    return;
+  }
+
+  const selectedRace = document.querySelector('input[name="field"]:checked');
+  if (!selectedRace) {
+    alert('Выберите расу');
+    return;
+  }
+
+  let status;
+  if (selectedRace.id === 'mc1') status = raceStats.human;
+  else if (selectedRace.id === 'mc2') status = raceStats.elf;
+  else if (selectedRace.id === 'mc3') status = raceStats.dwarf;
+
+  userChar.name = charInputName.value.trim();
+  userChar.type = status.type;
+  userChar.health = status.health;
+  userChar.strength = status.strength;
+  userChar.defense = status.defense;
+
+  startInfo.style.display = 'none';
+  rpgWrapper.style.display = 'flex';
+
+  updateCharInfo();
+});
+
+function updateCharInfo() {
+  const charList = document.querySelector('.char__info-list');
+  charList.innerHTML = '';
+
+  for (let key in userChar) {
+    const listItem = document.createElement('li');
+    listItem.className = '.char__info-item';
+
+    let value = userChar[key];
+    if (key === 'inventory') {
+      value = value.length === 0 ? 'Пусто' : value.join(', ');
+    }
+    listItem.textContent = `${propertyNames[key]}: ${value}`;
+    charList.append(listItem);
+  }
+
+}
--- script.js
+++ script.js
@@ -1,62 +1,161 @@
-const userChar = {
-    name: 'User',
-    type: 'Человек',
-    health: 100,
-    strength: 20,
-    defense: 15,
-    level: 1,
-    inventory: []
-};
-
-const charList = document.querySelector('.char__info-list');
-
-charList.innerHTML = '';
-const propertyNames = {
-    name: 'Имя',
-    type: 'Расса',
-    health: 'Здоровье',
-    strength: 'Сила',
-    defense: 'Защита',
-    level: 'Уровень',
-    inventory: 'Инвентарь'
-};
-for (let key in userChar) {
-    const listItem = document.createElement('li');
-    listItem.className = 'char__info-item';
-
-    let value = userChar[key];
-
-    if (key === 'inventory') {
-        value = value.length === 0 ? 'Пусто' : value.join(', ');
-    }
-
-    listItem.textContent = `${propertyNames[key]} : ${value}`;
-    charList.append(listItem);
-}
+const locationButtons = document.querySelectorAll('.loc-btn');
 const locationNow = document.querySelector('.location__now');
 const locations = document.querySelector('.location__buttons');
+const controlsBtns = document.querySelectorAll('.cnt-btn');
+const consoleEvent = document.querySelector('.rpg__event-list');
+let inCombat = false;
+let currentEnemy = null;
 
 function getLocationName(elem) {
-    if (elem.classList.contains('location__wood')) return 'Лес';
-    if (elem.classList.contains('location__dungeon')) return 'Подземелье';
-    if (elem.classList.contains('location__village')) return 'Деревня';
-    return null;
+  if (elem.classList.contains('location__wood')) return 'Лес';
+  if (elem.classList.contains('location__dungeon')) return 'Подземелье';
+  if (elem.classList.contains('location__village')) return 'Деревня';
+  return null;
+}
+
+function disabledButtons(buttons, disabled) {
+  buttons.forEach(btn => {
+    if (inCombat) {
+      btn.disabled = '';
+    } else {
+      btn.disabled = disabled;
+    }
+
+  });
+}
+
+function toggleLocButtons(btns) {
+  btns.forEach(btn => {
+    btn.style.display = 'none';
+  });
+}
+
+function setMessage(place, mess) {
+  const messageEvent = document.createElement('div');
+  messageEvent.textContent = mess;
+  place.append(messageEvent);
 }
 
 locations.addEventListener('click', (e) => {
-    const locationName = getLocationName(e.target);
-    const consoleEvent = document.querySelector('.rpg__event-list');
-    const messageEvent = document.createElement('div');
-    const locationButtons = document.querySelectorAll('.location__wood, .location__dungeon, .location__village');
-    if (locationName) {
-        locationNow.textContent = `Текущая локация: ${locationName}`;
-    }
-    if (locationName === 'Лес') {
-        locationButtons.forEach(btn => {
-            btn.setAttribute('disabled', '')
-        })
-        consoleEvent.append(messageEvent);
-        messageEvent.textContent = '...На вас нападает Орк';
-    }
+  if (inCombat) return;
+
+  const locationName = getLocationName(e.target);
+
+  if (locationName) {
+    locationNow.textContent = `Текущая локация: ${locationName}`;
+  }
+
+  if (locationName === 'Лес') {
+    toggleLocButtons(locationButtons);
+    const locationWays = document.querySelector('.location__ways');
+    locationWays.style.display = 'flex';
+    setMessage(consoleEvent, `...В дали слышен шум`);
+
+    locationWays.addEventListener('click', (e) => {
+      if (e.target.classList.contains('location__wood')) {
+        setMessage(consoleEvent, `...Из-за дерева выходит огромное туловище)`);
+        locationWays.style.display = 'none';
+        startCombat('orc');
+      } else if (e.target.classList.contains('location__bush')) {
+        setMessage(consoleEvent, `...Вы нашли Зелье здоровья!`);
+        userChar.inventory.push('Зелье здоровья');
+        updateCharInfo();
+        e.target.disabled = true;
+        e.target.style.opacity = '0.5';
+      }
+    });
+  }
 });
+
+function startCombat(enemyType) {
+  inCombat = true;
+  currentEnemy = {...enemies[enemyType]}; // Копируем врага
+  disabledButtons(locationButtons, true); // Блокируем кнопки локаций
+  disabledButtons(controlsBtns, false); // Разблокируем кнопки боя
+  setMessage(consoleEvent,
+      `...Это ${currentEnemy.name}! Здоровье: ${currentEnemy.health}`);
+
+  const attackBtn = document.querySelector('.controls__attack');
+  const defenseBtn = document.querySelector('.controls__defense');
+
+  attackBtn.addEventListener('click', handleAttack);
+  defenseBtn.addEventListener('click', handleDefense);
+}
+
+function handleAttack() {
+  if (!inCombat) {
+    return;
+  }
+  const damage = Math.max(1, userChar.strength - currentEnemy.defense);
+  currentEnemy.health -= damage;
+
+  setMessage(consoleEvent,
+      `*${userChar.name} атакует и наносит ${damage} урона!`);
+
+  if (currentEnemy.health <= 0) {
+    endCombat(true);
+    return;
+  }
+
+  setTimeout(() => enemyTurn(), 1000);
+  // endCombat();
+}
+
+function handleDefense() {
+  if (!inCombat) return;
+
+  // Уменьшаем получаемый урон при защите
+  const defenseBonus = 2;
+  setMessage(consoleEvent, `*${userChar.name} готовится к защите!`);
+
+  // Ход врага с бонусом защиты
+  setTimeout(() => enemyTurn(defenseBonus), 1000);
+}
+
+function enemyTurn(defenseBonus = 0) {
+  if (!inCombat) return;
+
+  // Урон врага с учетом защиты
+  const damage = Math.max(1,
+      currentEnemy.strength - (userChar.defense + defenseBonus));
+  userChar.health -= damage;
+  updateCharInfo();
+
+  setMessage(consoleEvent,
+      `-${currentEnemy.name} атакует и наносит ${damage} урона!`);
+
+  // Проверяем поражение
+  if (userChar.health <= 0) {
+    endCombat(false);
+    return;
+  }
+
+  setMessage(consoleEvent,
+      `Ваше здоровье: ${userChar.health}. Здоровье ${currentEnemy.name}: ${currentEnemy.health}`);
+}
+
+function endCombat(victory) {
+  inCombat = false;
+  disabledButtons(controlsBtns, true); // Блокируем кнопки боя
+  disabledButtons(locationButtons, false); // Разблокируем кнопки локаций
+
+  if (victory) {
+    setMessage(consoleEvent, `Вы победили ${currentEnemy.name}!`);
+    for (let key in userChar) {
+      if (key !== 'name' && key !== 'type') {
+        userChar[key] += 1
+      }
+    }
+    updateCharInfo()
+  } else {
+    setMessage(consoleEvent, `Вы потерпели поражение...`);
+
+  }
+
+  currentEnemy = null;
+}
+
+
+
+
 
